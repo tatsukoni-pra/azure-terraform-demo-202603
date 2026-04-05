502090c05ec748472ddc5008a2ff409574c59599
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2026-04-06 00:56:51 +0900

    feat

--- cosmosdb/feat/sql_container.tf
+++ cosmosdb/feat/sql_container.tf
@@ -13,3 +13,11 @@ resource "azurerm_cosmosdb_sql_container" "featcontainerfront" {
   database_name       = azurerm_cosmosdb_sql_database.featdatabase.name
   partition_key_paths = ["/userId"]
 }
+
+resource "azurerm_cosmosdb_sql_container" "featcontainerinfra" {
+  name                = "FeatContainerInfra"
+  resource_group_name = var.resource_group.name
+  account_name        = azurerm_cosmosdb_account.cosmosdb_account_feat.name
+  database_name       = azurerm_cosmosdb_sql_database.featdatabase.name
+  partition_key_paths = ["/tenantId"]
+}
