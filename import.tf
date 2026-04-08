6f3c5ff34e8c54b94bc133b755ba2051a229dd73
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2026-04-09 01:49:12 +0900

    import

---
+++ import.tf
@@ -0,0 +1,4 @@
+import {
+  to = azurerm_storage_account.tfstate
+  id = "/subscriptions/ba29533e-1e4c-43a8-898a-a5815e9b577b/resourceGroups/rg-tatsukoni-stg/providers/Microsoft.Storage/storageAccounts/sttatsukonistgtfstate"
+}
